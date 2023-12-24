3d0268b0ad87dfafe1a5fb9765556c9bc46003a3
Dimbb <bogomolovdmitrij535@gmail.com>
2023-12-24 13:14:28 +0300

    added js modal and drop-downs, changes in adaptive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,26 @@
-var slider = document.querySelector(".modal-window__range");
+// Variables
+
+// Range Variables
+
+const slider = document.querySelector(".modal-window__range");
+
+// Drop-Downs Variables
+
+const dropDowns = document.querySelectorAll(".drop-down-btn");
+
+// Modal Windows Variables
+
+const buttons = document.querySelectorAll("button");
+const modalWindows = document.querySelectorAll("div[data-modal]");
+
+// Functions
+
+function toggleDropDown(elem, currentEl, activeCl) {
+    elem.classList.toggle("none");
+    currentEl.classList.toggle(activeCl);
+}
+
+// Range Events
 
 noUiSlider.create(slider, {
     start: [20, 80],
@@ -7,4 +29,30 @@ noUiSlider.create(slider, {
         'min': 0,
         'max': 100
     }
+});
+
+// Drop-Downs Events
+
+dropDowns.forEach(item => {
+    item.addEventListener("click", (event) => {
+        const dropDown = event.currentTarget.nextElementSibling;
+
+        toggleDropDown(dropDown, event.currentTarget, "active-btn");
+    });
+});
+
+buttons.forEach(item => {
+    item.addEventListener("click", (event) => {
+        const currentBtn = event.currentTarget;
+
+        if (currentBtn.getAttribute("data-modal")) {
+            Array.from(modalWindows).forEach(modal => modal.classList.add("none"));
+            
+            Array.from(modalWindows).forEach(modal => {
+                if (modal.getAttribute("data-modal") == currentBtn.getAttribute("data-modal")) {
+                    modal.classList.remove("none");
+                }
+            });
+        }
+    });
 });
